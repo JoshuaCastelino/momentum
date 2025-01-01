6602fabdb42d370abe5c15d3f5d0e24e83dc94ef
JoshuaCastelino <67103454+JoshuaCastelino@users.noreply.github.com>
2025-01-01 16:21:20 +0000

    Initial Commit

---
+++ momentumTests/momentumTests.swift
@@ -0,0 +1,17 @@
+//
+//  momentumTests.swift
+//  momentumTests
+//
+//  Created by Josh on 01/01/2025.
+//
+
+import Testing
+@testable import momentum
+
+struct momentumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
